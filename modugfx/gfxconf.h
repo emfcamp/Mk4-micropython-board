4293dc8b673b4e65e101836220f2a8d16c4fb692
Matt Brejza <mbrejza@gmail.com>
2018-08-19 17:39:15 +0100

    adding experimental ugfx jpg decoder from latest ugfx repo

--- modugfx/gfxconf.h
+++ modugfx/gfxconf.h
@@ -116,7 +116,7 @@
 //        #define GDISP_NEED_IMAGE_BMP_16              TRUE
         #define GDISP_NEED_IMAGE_BMP_24              TRUE
 //        #define GDISP_NEED_IMAGE_BMP_32              TRUE
-//    #define GDISP_NEED_IMAGE_JPG                     FALSE
+    #define GDISP_NEED_IMAGE_JPG                     TRUE
 //    #define GDISP_NEED_IMAGE_PNG                     TRUE
 //        #define GDISP_NEED_IMAGE_PNG_INTERLACED      FALSE
 //        #define GDISP_NEED_IMAGE_PNG_TRANSPARENCY    TRUE
