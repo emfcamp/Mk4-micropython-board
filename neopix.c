22532ba9656f2b6269f4ff6db56e38af81b07f17
Matt Brejza <mbrejza@gmail.com>
2018-08-25 17:49:47 +0100

    neopix done

--- neopix.c
+++ neopix.c
@@ -1,4 +1,4 @@
- /*
+/*
  *
  * The MIT License (MIT)
  *
@@ -24,6 +24,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include <stdlib.h>
 
 
 #include "py/nlr.h"
@@ -36,15 +37,15 @@
 #include "ti/devices/msp432e4/driverlib/driverlib.h"
 #include <ti/drivers/Power.h>
 #include <ti/drivers/power/PowerMSP432E4.h>
+#include <ti/drivers/dpl/HwiP.h>
+
 
 
 /// \moduleref Neopix
 /// UPDATE ME
 
-static volatile uint16_t * frame_buffer;
-static uint16_t frame_buffer_[10*24];
-static uint16_t dmabuff[] = {10,20,30,40,50,60,70,80,90,11,99,11,99,11,149};
-static volatile uint8_t ws_xr_active;
+static uint16_t * frame_buffer = NULL;
+static uint32_t frame_buffer_size = 0;
 
 #define WS_800HZ 800000
 #define WS_400HZ 400000
@@ -64,69 +65,23 @@ typedef struct _pyb_neopix_t {
 
 /// \classmethod \constructor(pin)
 ///
-/// Construct an Neopix object. Needs a pin object as input. Pin has to have a TIM15 CH1N output
+/// Construct an Neopix object. 
 STATIC mp_obj_t pyb_neopix_make_new(const mp_obj_type_t *type, mp_uint_t n_args, mp_uint_t n_kw, const mp_obj_t *args) {
     // check arguments
-    mp_arg_check_num(n_args, n_kw, 1, 1, false);
+    mp_arg_check_num(n_args, n_kw, 0, 0, false);
 
     // create object
     pyb_neopix_obj_t *neo = m_new_obj(pyb_neopix_obj_t);
     neo->base.type = &pyb_neopix_type;
-
-/*	mp_obj_t pin_obj = args[0];
-	if (!MP_OBJ_IS_TYPE(pin_obj, &pin_type)) {
-		nlr_raise(mp_obj_new_exception_msg(&mp_type_ValueError, "pin argument needs to be be a Pin type"));
-	}
-	const pin_obj_t *pin = pin_obj;
-	const pin_af_obj_t *af = pin_find_af(pin, AF_FN_TIM, 15);
-	if (af == NULL) {
-		nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_ValueError, "pin %q doesn't have an af for TIM%d", pin->name, 15));
-	}
-	const mp_obj_t args2[6] = {
-		(mp_obj_t)&pin_init_obj,
-		pin_obj,
-		MP_OBJ_NEW_QSTR(MP_QSTR_mode),  MP_OBJ_NEW_SMALL_INT(GPIO_MODE_AF_PP),
-		MP_OBJ_NEW_QSTR(MP_QSTR_af),    MP_OBJ_NEW_SMALL_INT(af->idx)
-	};
-	mp_call_method_n_kw(0, 2, args2);*/
     
-    ws_xr_active = 0;
     setup_ws_timer_dma();
-	
-    /*
-	tim.Instance = TIM15;
-	tim.Init.Prescaler = 0;
-	tim.Init.Period = 99;
-	tim.Init.CounterMode = TIM_COUNTERMODE_UP;
-	tim.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-	tim.Init.RepetitionCounter = 0;
-	__TIM15_CLK_ENABLE();
-
-	
-	HAL_TIM_Base_Init(&tim);
-
-	
-	TIM_OC_InitTypeDef oc_config;
-	oc_config.OCMode = TIM_OCMODE_PWM1;
-	oc_config.Pulse = 0;
-	
-	oc_config.OCPolarity   = TIM_OCPOLARITY_HIGH;
-	oc_config.OCNPolarity  = TIM_OCNPOLARITY_LOW;
-	oc_config.OCFastMode   = TIM_OCFAST_DISABLE;
-	oc_config.OCIdleState  = TIM_OCIDLESTATE_SET;
-	oc_config.OCNIdleState = TIM_OCNIDLESTATE_RESET;
-	
-	HAL_TIM_PWM_ConfigChannel(&tim, &oc_config, 0); //TIM_CHANNEL_1);
-	HAL_TIM_PWM_Start(&tim, 0);
-	HAL_TIMEx_PWMN_Start(&tim, 0);
-	*/
 	
 	return neo;
 }
 
 // called when the dma transfer is complete
 void
-TIMER3B_IRQHandler(void)
+TIMER3B_IRQHandler(unsigned int a)
 {
     uint32_t getTimerIntStatus;
 
@@ -134,7 +89,6 @@ TIMER3B_IRQHandler(void)
     getTimerIntStatus = MAP_TimerIntStatus(TIMER3_BASE, true);
     MAP_TimerIntClear(TIMER3_BASE, getTimerIntStatus);
 
-    ws_xr_active = 0;
     MAP_TimerDisable(TIMER3_BASE, TIMER_BOTH);
     
 }
@@ -151,7 +105,7 @@ static void setup_ws_timer_dma(void)
 
     MAP_TimerLoadSet(TIMER3_BASE, TIMER_BOTH, WS2812_TIMER_INTVAL);
 
-    MAP_TimerMatchSet(TIMER3_BASE, TIMER_A, WS2812_TIMER_INTVAL-1);
+    MAP_TimerMatchSet(TIMER3_BASE, TIMER_A, WS2812_DUTYCYCLE_RESET);
     
     
     HwiP_create(INT_TIMER3B, TIMER3B_IRQHandler, NULL);
@@ -175,26 +129,14 @@ static void setup_ws_timer_dma(void)
                                   UDMA_SIZE_16 | UDMA_SRC_INC_16 | UDMA_DST_INC_NONE |
                                   UDMA_ARB_1);
 
-    //remove...
-    /*
-    MAP_uDMAChannelTransferSet(UDMA_CH3_TIMER3B | UDMA_PRI_SELECT,
-                                   UDMA_MODE_BASIC,
-                                   (void *)&dmabuff, (void*)&TIMER3->TAMATCHR,
-                                   sizeof(dmabuff)/sizeof(uint16_t));
-                                   
 
-    //MAP_IntEnable(INT_TIMER3B);
-    MAP_TimerEnable(TIMER3_BASE, TIMER_BOTH);
-
-    MAP_uDMAChannelEnable(UDMA_CH3_TIMER3B);*/
 }
 
-void ws_start_transfer(uint16_t * fb, uint32_t len){
-                                  
-    frame_buffer = fb;    
+void ws_start_transfer( uint32_t len){
+                            
     MAP_uDMAChannelTransferSet(UDMA_CH3_TIMER3B | UDMA_PRI_SELECT,
                                    UDMA_MODE_BASIC,
-                                   (void *)&frame_buffer_, (void*)&TIMER3->TAMATCHR,
+                                   (void *)frame_buffer, (void*)&TIMER3->TAMATCHR,
                                    len);
     //MAP_uDMAChannelTransferSet(UDMA_CH3_TIMER3B | UDMA_PRI_SELECT,
     //                               UDMA_MODE_BASIC,
@@ -206,7 +148,6 @@ void ws_start_transfer(uint16_t * fb, uint32_t len){
     MAP_TimerEnable(TIMER3_BASE, TIMER_BOTH);
 
     MAP_uDMAChannelEnable(UDMA_CH3_TIMER3B);
-    ws_xr_active = 1;
     
 }
 
@@ -221,6 +162,7 @@ STATIC mp_obj_t pyb_neopix_display(mp_obj_t self_in, mp_obj_t rgb) {
 	int tx;
 	int mask;
 	int val;
+    
 	
 	mp_obj_t *items;
 	
@@ -228,11 +170,20 @@ STATIC mp_obj_t pyb_neopix_display(mp_obj_t self_in, mp_obj_t rgb) {
 		len = 1;
 	else
 		mp_obj_get_array(rgb, &len, &items);
-	
-    
+
     uint32_t buf_ptr = 0;
+
+    if (frame_buffer_size < (24*len+2)*sizeof(uint16_t)){
+        if (frame_buffer != NULL)
+            free(frame_buffer);
+        frame_buffer = malloc((24*len+2)*sizeof(uint16_t));
+        if (frame_buffer == NULL)
+            return mp_const_none;
+        frame_buffer_size = (24*len+2)*sizeof(uint16_t);
+    }
+
     
-    frame_buffer_[buf_ptr++] = 149;
+    frame_buffer[buf_ptr++] = WS2812_DUTYCYCLE_RESET;
 	
 	while(len){
 		len--;
@@ -247,17 +198,17 @@ STATIC mp_obj_t pyb_neopix_display(mp_obj_t self_in, mp_obj_t rgb) {
 		while (mask){
 						
 			if (mask & tx)
-				frame_buffer_[buf_ptr++] = WS2812_DUTYCYCLE_1;
+				frame_buffer[buf_ptr++] = WS2812_DUTYCYCLE_1;
 			else
-				frame_buffer_[buf_ptr++] = WS2812_DUTYCYCLE_0;
+				frame_buffer[buf_ptr++] = WS2812_DUTYCYCLE_0;
 						
 			mask = mask >> 1;
 		}
 	}
      
-    frame_buffer_[buf_ptr++] = 149;
+    frame_buffer[buf_ptr++] = WS2812_DUTYCYCLE_RESET;
      
-	ws_start_transfer(frame_buffer_, buf_ptr);
+	ws_start_transfer( buf_ptr);
 	
 	return mp_const_none;
 }
@@ -267,9 +218,9 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_2(pyb_neopix_display_obj, pyb_neopix_display);
 ///
 /// Stops the timer
 STATIC mp_obj_t pyb_neopix_destroy(mp_obj_t self_in) {
-	//tim.State = HAL_TIM_STATE_RESET;
-	//tim.Instance->CCER = 0x0000; // disable all capture/compare outputs
-	//tim.Instance->CR1 = 0x0000; // disable the timer and reset its state
+	free(frame_buffer);
+    frame_buffer = NULL;
+    frame_buffer_size = 0;
 	return mp_const_none;
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(pyb_neopix_destroy_obj, pyb_neopix_destroy);	
