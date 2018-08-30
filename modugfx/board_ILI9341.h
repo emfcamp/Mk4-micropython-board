73ffe37fd8155c2a92bb8a9563225ff93eb046d7
Mark Grosen <mark@grosen.org>
2018-08-30 10:27:59 +0100

    Performance tweaks for ILI9341 display driver
    This uses direct access to the SPI registers to optimize the
    one byte transfers for data and cmd. Also tweaks copy loop
    for slight improvement. These make approximately 25% improvement
    in a couple small synthetic benchmarks - YMMV. This optimization
    is SoC-specific since most devices do not share a common
    SPI controller.
    
    Bumping the SPI clock to 30MHz helps fills/clears look much
    crisper.
    
    Noticed that the size of the DMA buffer seems to make a difference,
    but not intuitively to me. I thought a size of 320 or 240 would
    be best but some testing seems to indicate not.

--- modugfx/board_ILI9341.h
+++ modugfx/board_ILI9341.h
@@ -8,24 +8,43 @@
 #ifndef _GDISP_LLD_BOARD_H
 #define _GDISP_LLD_BOARD_H
 
-//#include STM32_HAL_H
 #include "py/mphal.h"
+
 #include <ti/drivers/SPI.h>
+#include <ti/drivers/spi/SPIMSP432E4DMA.h>
 #include <ti/drivers/GPIO.h>
 #include <ti/drivers/PWM.h>
 
+#include <ti/devices/msp432e4/driverlib/ssi.h>
 
-SPI_Handle spi_h;
-PWM_Handle pwm_h;
+#define SPI_DIRECT 1
 
-SPI_Transaction trans;
-#define DMA_BUFF_LEN 100
-uint8_t dma_buffer[DMA_BUFF_LEN*2];
-uint16_t dma_buffer_i;
+#if SPI_DIRECT
+static uint32_t spiBase = 0x0;
+#endif
+
+static SPI_Handle spi_h;
+#ifdef MICROPY_HW_UGFX_BL_PWM
+static PWM_Handle pwm_h;
+#endif
+
+static SPI_Transaction trans;
+
+#define DMA_BUFF_LEN 80
+static uint8_t dma_buffer[DMA_BUFF_LEN*2];
+static uint16_t dma_buffer_i;
 
 extern orientation_t blit_rotation;
 
-
+#if SPI_DIRECT
+static inline void Direct_write(uint32_t base, uint32_t value)
+{
+    SSIEnable(spiBase);
+    SSIDataPut(spiBase, value);
+    while (SSIBusy(spiBase));
+    SSIDisable(spiBase);
+}
+#endif
 
 static GFXINLINE void change_spi_speed(uint32_t clk) {
     SPI_close(spi_h);
@@ -34,36 +53,37 @@ static GFXINLINE void change_spi_speed(uint32_t clk) {
     params.bitRate = clk;
     params.dataSize = 8;
     params.frameFormat = 0;
-    //params.transferMode = SPI_MODE_CALLBACK;
-    spi_h = SPI_open(MICROPY_HW_UGFX_SPI, &params);   
+    spi_h = SPI_open(MICROPY_HW_UGFX_SPI, &params);
 }
 
 
 static GFXINLINE void init_board(GDisplay *g) {
-	// As we are not using multiple displays we set g->board to NULL as we don't use it.
-
+    // As we are not using multiple displays we set g->board to NULL as we don't use it.
+    //g->board = NULL;
 
     SPI_Params params;
     SPI_Params_init(&params);
-    params.bitRate = 10000000;
+    params.bitRate = 30000000;
     params.dataSize = 8;
     params.frameFormat = 0;
-    //params.transferMode = SPI_MODE_CALLBACK;
-
 
     spi_h = SPI_open(MICROPY_HW_UGFX_SPI, &params);
-    
+
+#if SPI_DIRECT
+    spiBase = ((SPIMSP432E4DMA_HWAttrs *)(spi_h->hwAttrs))->baseAddr;
+#endif
+
 #ifdef MICROPY_HW_UGFX_BL_PWM
-    PWM_Params pwmParams;    
+    PWM_Params pwmParams;
     PWM_Params_init(&pwmParams);
     pwmParams.idleLevel = PWM_IDLE_LOW;      // Output low when PWM is not running
     pwmParams.periodUnits = PWM_PERIOD_HZ;   // Period is in Hz
     pwmParams.periodValue = 10000;           // 10kHz
     pwmParams.dutyUnits = PWM_DUTY_FRACTION; // Duty is in fractional percentage
     pwmParams.dutyValue = PWM_DUTY_FRACTION_MAX/2; // 50% initial duty cycle
-    
+
     pwm_h = PWM_open(MICROPY_HW_UGFX_BL_PWM, &pwmParams);
-    
+
     PWM_start(pwm_h);
 #else
 #ifdef MICROPY_HW_UGFX_PIN_BL
@@ -71,12 +91,9 @@ static GFXINLINE void init_board(GDisplay *g) {
 #endif
 #endif
 
-    
     GPIO_write(MICROPY_HW_UGFX_PIN_CS, 1);
     GPIO_write(MICROPY_HW_UGFX_PIN_RST, 1);
     GPIO_write(MICROPY_HW_UGFX_PIN_A0, 1);
-
-
 }
 
 static GFXINLINE void post_init_board(GDisplay *g) {
@@ -88,145 +105,135 @@ static inline void set_blit_rotation(orientation_t o){
 }
 
 static GFXINLINE void setpin_reset(GDisplay *g, bool_t state) {
-	(void) g;
-	if(state) {
-		// reset lcd
-		//GPIO_clear_pin(MICROPY_HW_UGFX_PORT_RST, MICROPY_HW_UGFX_PIN_RST);
-        //GPIOPinWrite(MICROPY_HW_UGFX_PORT_RST, MICROPY_HW_UGFX_PIN_RST, 0);
+    (void) g;
+    if (state) {
         GPIO_write(MICROPY_HW_UGFX_PIN_RST, 0);
-	} else {
-		//GPIO_set_pin(MICROPY_HW_UGFX_PORT_RST, MICROPY_HW_UGFX_PIN_RST);
-        //GPIOPinWrite(MICROPY_HW_UGFX_PORT_RST, MICROPY_HW_UGFX_PIN_RST, MICROPY_HW_UGFX_PIN_RST);
+    }
+    else {
         GPIO_write(MICROPY_HW_UGFX_PIN_RST, 1);
-	}
+    }
 }
 
-
 static GFXINLINE void set_backlight(GDisplay *g, uint8_t percent) {
-	(void) g;
+    (void) g;
 #ifdef MICROPY_HW_UGFX_BL_PWM
    PWM_setDuty(pwm_h,(PWM_DUTY_FRACTION_MAX / 100)*percent);
 #endif
 }
 
 static GFXINLINE void acquire_bus(GDisplay *g) {
-	(void) g;
+    (void) g;
 }
-
 
 static GFXINLINE void release_bus(GDisplay *g) {
-	(void) g;
-	//GPIOPinWrite(MICROPY_HW_UGFX_PORT_CS, MICROPY_HW_UGFX_PIN_CS, MICROPY_HW_UGFX_PIN_CS);  //CS high
+    (void) g;
     GPIO_write(MICROPY_HW_UGFX_PIN_CS, 1);
 }
-static GFXINLINE void write_index(GDisplay *g, uint16_t index) {
-	(void) g;
 
-	//GPIOPinWrite(MICROPY_HW_UGFX_PORT_CS, MICROPY_HW_UGFX_PIN_CS, 0);  //CS low
-	//GPIOPinWrite(MICROPY_HW_UGFX_PORT_A0, MICROPY_HW_UGFX_PIN_A0, 0);  //CMD low
+static GFXINLINE void write_index(GDisplay *g, uint16_t index) {
+    (void) g;
+
     GPIO_write(MICROPY_HW_UGFX_PIN_CS, 0);
     GPIO_write(MICROPY_HW_UGFX_PIN_A0, 0);
 
-	//HAL_SPI_Transmit(&ili_spi, &index, 1, 1000);
-
-
+    //System_printf("%x ", index);
+#if  SPI_DIRECT
+    Direct_write(spiBase, index);
+#else
     trans.txBuf = &index;
     trans.rxBuf = NULL;
     trans.count = 1;
 
     (void)SPI_transfer(spi_h, &trans);
-	//GPIOPinWrite(MICROPY_HW_UGFX_PORT_A0, MICROPY_HW_UGFX_PIN_A0, MICROPY_HW_UGFX_PIN_A0);  //CMD high
+#endif
+
     GPIO_write(MICROPY_HW_UGFX_PIN_A0, 1);
     GPIO_write(MICROPY_HW_UGFX_PIN_CS, 1);
 }
+
 static GFXINLINE void write_data(GDisplay *g, uint16_t data) {
-	(void) g;
-	//HAL_SPI_Transmit(&ili_spi, &data, 1, 1000);
-GPIO_write(MICROPY_HW_UGFX_PIN_CS, 0);
+    (void) g;
+
+    GPIO_write(MICROPY_HW_UGFX_PIN_CS, 0);
+
+#if SPI_DIRECT
+    Direct_write(spiBase, data);
+#else
     trans.txBuf = &data;
     trans.rxBuf = NULL;
     trans.count = 1;
     (void)SPI_transfer(spi_h, &trans);
-    //GPIO_write(MICROPY_HW_UGFX_PIN_CS, 1);
+#endif
 }
-   
+
 static GFXINLINE void write_data16_block_flush(GDisplay *g) {
    (void) g;
-      
+
    GPIO_write(MICROPY_HW_UGFX_PIN_CS, 0);
-   
+
    trans.txBuf = &dma_buffer;
-   trans.rxBuf = NULL;           
+   trans.rxBuf = NULL;
    trans.count = dma_buffer_i;
    (void)SPI_transfer(spi_h, &trans);
-   
+
    dma_buffer_i = 0;
-   
+
    //GPIO_write(MICROPY_HW_UGFX_PIN_CS, 1);
 }
 
 static GFXINLINE void write_data16_block(GDisplay *g, uint16_t data) {
-      
+
    dma_buffer[dma_buffer_i++] = data>>8;
    dma_buffer[dma_buffer_i++] = data&0xFF;
-   
+
    if (dma_buffer_i >= DMA_BUFF_LEN*2)
       write_data16_block_flush(g);
 }
 
 static GFXINLINE void write_data16_repeated(GDisplay *g, uint16_t data, uint32_t cnt) {
-	(void) g;
-   
-   GPIO_write(MICROPY_HW_UGFX_PIN_CS, 0);   
-   
-   uint8_t c_low = data&0xff;
-   uint8_t c_high = data>>8;
-   for (int i = 0; i < DMA_BUFF_LEN*2;){
-      dma_buffer[i] = c_high;
-      i++;
-      dma_buffer[i] = c_low;
-      i++;
+    (void) g;
+
+   GPIO_write(MICROPY_HW_UGFX_PIN_CS, 0);
+
+   uint16_t be_data = ((data & 0xff00) >> 8) | ((data & 0xff) << 8);
+   uint16_t * buf = (uint16_t *)dma_buffer;
+   for (uint32_t i = 0; i < DMA_BUFF_LEN; i++) {
+       *buf++ = be_data;
    }
-   
+
    trans.txBuf = &dma_buffer;
    trans.rxBuf = NULL;
-   
-   while(cnt >= DMA_BUFF_LEN){         
-      trans.count = DMA_BUFF_LEN*2;
+   trans.count = DMA_BUFF_LEN * 2;
+
+   while(cnt >= DMA_BUFF_LEN){
       (void)SPI_transfer(spi_h, &trans);
       cnt -= DMA_BUFF_LEN;
    }
-   trans.count = cnt*2;
+   trans.count = cnt * 2;
    (void)SPI_transfer(spi_h, &trans);
 
-   
    //GPIO_write(MICROPY_HW_UGFX_PIN_CS, 1);
-   
-   
 }
 
 static GFXINLINE uint16_t read_data(GDisplay *g) {
-	(void) g;
-	uint8_t d;
-	//HAL_SPI_Receive(&ili_spi, &d, 1, 1000);
+    (void) g;
+    uint8_t d;
+
     SPI_Transaction trans;
     trans.txBuf = NULL;
     trans.rxBuf = &d;
     trans.count = 1;
     (void)SPI_transfer(spi_h, &trans);
-	return d;
+
+    return d;
 }
 
-
 static GFXINLINE void setreadmode(GDisplay *g) {
-	(void) g;
+    (void) g;
 }
 
 static GFXINLINE void setwritemode(GDisplay *g) {
-	(void) g;
+    (void) g;
 }
-
-
-
 
 #endif /* _GDISP_LLD_BOARD_H */
